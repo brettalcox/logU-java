03ba6b0864ed5b24db89d44396555d2b9931bf5c
Brett Alcox <brettalcox@gmail.com>
2016-04-14 15:59:14 -0500

    added preference methods for the settings view/form

--- app/src/main/java/com/loguapp/logu_java/Prefs.java
+++ app/src/main/java/com/loguapp/logu_java/Prefs.java
@@ -9,6 +9,9 @@ import android.content.SharedPreferences;
 public class Prefs {
     private static String USERNAME = "username";
     private static String ISLOGGEDIN = "loggedIn";
+    private static String BODYWEIGHT = "bodyweight";
+    private static String GENDER = "gender";
+    private static String UNIT = "unit";
 
     private static SharedPreferences getPrefs(Context context) {
         return context.getSharedPreferences("MyPreferences", 0);
@@ -33,5 +36,29 @@ public class Prefs {
 
     public static void userLoggedOut(Context context) {
         getPrefs(context).edit().putInt(ISLOGGEDIN, 0).apply();
+    }
+
+    public static void setBodyweight(Context context, int value) {
+        getPrefs(context).edit().putInt(BODYWEIGHT, value).apply();
+    }
+
+    public static void setGender(Context context, int value) {
+        getPrefs(context).edit().putInt(GENDER, value).apply();
+    }
+
+    public static void setUnit(Context context, int value) {
+        getPrefs(context).edit().putInt(UNIT, value).apply();
+    }
+
+    public static int getBodyweight(Context context) {
+        return getPrefs(context).getInt(BODYWEIGHT, 9000);
+    }
+
+    public static String getGender(Context context) {
+        return getPrefs(context).getString(GENDER, "Unknown");
+    }
+
+    public static String getUnit(Context context) {
+        return getPrefs(context).getString(UNIT, "Unknown");
     }
 }
