9585a3782f5e3a42f312f0864e5f0b11ea6cd4b4
Brett Alcox <brettalcox@gmail.com>
2016-04-15 15:23:15 -0500

    network methods added, and now coords from the database show clustered on the map.

--- app/src/main/java/com/loguapp/logu_java/ClusteringActivity.java
+++ app/src/main/java/com/loguapp/logu_java/ClusteringActivity.java
@@ -1,8 +1,22 @@
 package com.loguapp.logu_java;
 
+import android.os.AsyncTask;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by BA042808 on 4/15/2016.
@@ -10,6 +24,7 @@ import com.google.maps.android.clustering.ClusterManager;
 public class ClusteringActivity extends CommunityActivity {
 
     ClusterManager<MapItemCluster> mClusterManager;
+    private JSONObject[] gpsCoords;
 
     protected void setUpClusterer() {
         // Declare a variable for the cluster manager.
@@ -32,17 +47,82 @@ public class ClusteringActivity extends CommunityActivity {
 
     private void addItems() {
 
-        // Set some lat/lng coordinates to start with.
-        double lat = 51.5145160;
-        double lng = -0.1270060;
+        try {
+            gpsCoords = new GymMapMarkers().execute("https://loguapp.com/request_coords.php").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
-        // Add ten cluster items in close proximity, for purposes of this example.
-        for (int i = 0; i < 100; i++) {
-            double offset = i / 60d;
-            lat = lat + offset;
-            lng = lng + offset;
-            MapItemCluster offsetItem = new MapItemCluster(lat, lng);
-            mClusterManager.addItem(offsetItem);
+        int lat;
+        int lng;
+
+        for (int i = 0; i < gpsCoords.length; i++) {
+            try {
+                lat = gpsCoords[i].getInt("latitude");
+                lng = gpsCoords[i].getInt("longitude");
+                MapItemCluster item = new MapItemCluster(lat, lng);
+                mClusterManager.addItem(item);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class GymMapMarkers extends AsyncTask<String, Void, JSONObject[]> {
+        @Override
+        protected JSONObject[] doInBackground(String... url) {
+
+            JSONObject[] gps_coords = {};
+
+            try {
+                URL obj = new URL(url[0]);
+                HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+                con.setRequestMethod("POST");
+                con.setRequestProperty("accept", "application/json");
+                con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+                String urlParameters = "none=none";
+
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(urlParameters);
+                wr.flush();
+                wr.close();
+
+                int responseCode = con.getResponseCode();
+                System.out.println("\nSending 'POST' request to URL : " + url);
+                System.out.println("Post parameters : " + urlParameters);
+                System.out.println("Response Code : " + responseCode);
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                JSONArray json_data = new JSONArray(new String(response.toString()));
+                JSONObject object = new JSONObject();
+
+                JSONObject[] user_data = new JSONObject[json_data.length()];
+
+                for(int i=0; i < json_data.length(); i++)
+                {
+                    object = json_data.getJSONObject(i);
+                    user_data[i] = object;
+                }
+                gps_coords = user_data;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return gps_coords;
         }
     }
 }
