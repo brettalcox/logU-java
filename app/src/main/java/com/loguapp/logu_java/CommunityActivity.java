4a52a49bd7b17e33e6af990d47324d1371596308
Brett Alcox <brettalcox@gmail.com>
2016-04-15 10:04:23 -0500

    added the static listview to community page. implemented the method that makes the scroll view and listview play nice.

--- app/src/main/java/com/loguapp/logu_java/CommunityActivity.java
+++ app/src/main/java/com/loguapp/logu_java/CommunityActivity.java
@@ -5,21 +5,52 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 /**
  * Created by BA042808 on 3/31/2016.
  */
 public class CommunityActivity extends FragmentActivity {
 
+    ListView lview;
+    StaticListViewAdapter lviewAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_fragment);
 
-
         Fragment newFragment = new MapFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.mapView, newFragment);
         transaction.commit();
+
+        String[] staticList = {"Weekly Poundage Graph", "Weekly Poundage Data", "Targeted Muscle Graph"};
+        lview = (ListView) findViewById(R.id.staticCommListView);
+        lviewAdapter = new StaticListViewAdapter(CommunityActivity.this, staticList);
+        lview.setAdapter(lviewAdapter);
+        setListViewHeightBasedOnChildren(lview);
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        StaticListViewAdapter listAdapter = (StaticListViewAdapter) listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
