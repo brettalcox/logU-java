dfd79ff36114f2f9df3fc6445ce5691382fa8e13
Brett Alcox <brettalcox@gmail.com>
2016-04-15 00:21:07 -0500

    added mapview to community page

--- app/src/main/java/com/loguapp/logu_java/CommunityActivity.java
+++ app/src/main/java/com/loguapp/logu_java/CommunityActivity.java
@@ -1,17 +1,25 @@
 package com.loguapp.logu_java;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by BA042808 on 3/31/2016.
  */
-public class CommunityActivity extends Activity {
+public class CommunityActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_community);
+        setContentView(R.layout.map_fragment);
 
+
+        Fragment newFragment = new MapFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.mapView, newFragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/loguapp/logu_java/MapFragment.java
@@ -0,0 +1,57 @@
+package com.loguapp.logu_java;
+
+import android.app.Fragment;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by brettalcox on 4/14/16.
+ */
+public class MapFragment extends Fragment {
+
+    MapView m;
+    GoogleMap map;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.map_fragment, container, false);
+        m = (MapView) v.findViewById(R.id.mapView);
+        m.onCreate(savedInstanceState);
+
+        map = m.getMap();
+        //map.getUiSettings().setMyLocationButtonEnabled(false);
+
+        if (ActivityCompat.checkSelfPermission(container.getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(container.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            //return TODO;
+        }
+        //map.setMyLocationEnabled(true);
+
+        return v;
+    }
+
+    @Override
+    public void onResume() {
+        m.onResume();
+        super.onResume();
+    }
+
+}
